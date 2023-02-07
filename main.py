38ec0c3ec8d749100f88dc7486f3b8ff855574c0
Brody <bdsmith0010@zonemail.clpccd.edu>
2023-02-06 16:33:09 -0800

    lab 2

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 def main():
     ##################################################
     # Comlete your code here
+    reg_hours = int(40)
+    reg_rate = float(18.25)
+    o_rate = float(27.78)
+    workhours = int(input('How many hours did you work this week? '))
+    if workhours > reg_hours:
+        o_hours = workhours - reg_hours
+        reg_charge = reg_rate * reg_hours
+        o_charge = o_hours * o_rate
+        total_wage = reg_charge + o_charge
+    if workhours > reg_hours:
+        print ('Regular Charge : $', reg_charge,
+        '\nOvertime Charge : $', o_charge,
+        '\nTotal Wage : $', total_wage)
+    if workhours <= reg_hours:
+        print ('Total Wage : $', workhours * reg_rate)
     ##################################################
     pass
 
